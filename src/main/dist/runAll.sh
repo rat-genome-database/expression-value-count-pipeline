b3f613c6dd69960af4883fb6115d78d55536a85b
llamersmcw <llamers@mcw.edu>
2025-07-11 12:45:48 -0500

    updating for more memory

--- src/main/dist/runAll.sh
+++ src/main/dist/runAll.sh
@@ -8,6 +8,6 @@ cd $APPDIR
 
 java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
-    -jar lib/$APPNAME.jar -runForAll "$@" > run.log 2>&1
+    -Xmx20g -jar lib/$APPNAME.jar -runForAll "$@" > run.log 2>&1
 
 mailx -s "[$SERVER] expression-value-count-pipeline Run" llamers@mcw.edu < $APPDIR/logs/summary.log
--- src/main/dist/runForOne.sh
+++ src/main/dist/runForOne.sh
@@ -11,6 +11,6 @@ SPECIES=3
 
 java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
-    -jar lib/$APPNAME.jar $SPECIES "$@" > run.log 2>&1
+    -Xmx20g -jar lib/$APPNAME.jar $SPECIES "$@" > run.log 2>&1
 
 mailx -s "[$SERVER] expression-value-count-pipeline Run" llamers@mcw.edu < $APPDIR/logs/summary.log
